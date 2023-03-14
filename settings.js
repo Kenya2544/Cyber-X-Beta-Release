f4b580c3b93c9967c057ccb476447c37fc730b90
Kenya2544 <126442162+Kenya2544@users.noreply.github.com>
2023-03-14 19:04:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,10 +43,10 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['+254710504158']
+global.ownertag = '+254710504158' //ur tag number
+global.botnumber = '+254710504158' //Add your Bot Number For Log
+global.botname = 'VIBEZ KILLER' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
